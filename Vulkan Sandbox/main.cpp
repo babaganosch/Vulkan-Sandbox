f6917bc5a9871703a1e687092c329e269634b200
Babaganosch <lassemaja.92@gmail.com>
2020-06-05 20:38:05 +0200

    Initial Commit

---
+++ Vulkan Sandbox/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vulkan Sandbox
+//
+//  Created by Lars Andersson on 2020-06-05.
+//  Copyright © 2020 Lars Andersson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
